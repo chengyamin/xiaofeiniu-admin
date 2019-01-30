a2d6d7ab84c79328afa2958b13bd48c95120c00b
ChengYamin <chengyamimxiao@163.com>
2019-01-30 09:07:18 +0800

    项目管理子系统API

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+      <h3>{{1000 | date}}</h3>
+      <h3>{{1000 | datetime}}</h3>
+      <h3>{{1000 | currency}}</h3>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="">
+
+</style>
